9e7272a9c9f8448d00cb7038e858c81edc6da844
Liben Warsame <libenwarsame@Libens-MacBook-Pro.local>
2022-10-17 20:12:54 -0500

    updated routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,35 +2,41 @@ const router = require('express').Router();
 const { User, Pets } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-    try {
-        //get all user data and exclude id/pw
-      const userData = await User.findAll({
-        attributes: { exclude: ['id, password'] },
-        order: [['email', 'ASC']],
-      });
-      //get all pet data and exclude id/filename
-      const petData = await Pets.findAll({
-        attributes: { exclude: ['id, filename'] },
-        order: [['name', 'breed', 'description', 'age', 'ASC']],
-      })
+router.get('/', async (req, res) => {
+  try {
+    const petData = await Pets.findAll({
+    });
 
-      const users = userData.map((project) => project.get({ plain: true }));
-      const pets = petData.map((project) => project.get({ plain: true }));
+    const Pets = petData.map((Pets) => Pets.get({ plain: true }));
 
-      res.render('homepage', {
-        users,
-        pets,
-        logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+    res.render('homepage', { 
+      Pets, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/pets/:id', async (req, res) => {
+  try {
+    const petData = await Pets.findByPk(req.params.id, {
+    });
+
+    const Pets = petData.get({ plain: true });
+
+    res.render('petdetails', {
+      ...Pets,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/homepage');
     return;
   }
 
