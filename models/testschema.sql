cec1767a9ccece3c0683f35aa91bd5fe139ae1bb
DalPenny <105093756+DalPenny@users.noreply.github.com>
2022-10-17 21:40:54 -0500

    committing things

--- models/testschema.sql
+++ models/testschema.sql
@@ -1,26 +1,24 @@
 
-USE pets_db;
+-- USE pets_db;
 
-CREATE TABLE pet (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  breed VARCHAR(30) NOT NULL,
-  type VARCHAR(30) NOT NULL,
-  filename VARCHAR(30) NOT NULL,
-  description VARCHAR(30) NOT NULL,
-  age INT NOT NULL
-);
+-- CREATE TABLE pet (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(30) NOT NULL,
+--   breed VARCHAR(30) NOT NULL,
+--   type VARCHAR(30) NOT NULL,
+--   filename VARCHAR(30) NOT NULL,
+--   description VARCHAR(30) NOT NULL,
+--   age INT NOT NULL
+-- );
 
-USE user_db;
-
-CREATE TABLE user (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(30) NOT NULL,
-  first_name VARCHAR(30) ,
-  last_name VARCHAR(30),
-  email VARCHAR(30) NOT NULL,
-  token VARCHAR(30) ,
-  password VARCHAR(30) NOT NULL
-);
+-- CREATE TABLE user (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   username VARCHAR(30) NOT NULL,
+--   first_name VARCHAR(30) ,
+--   last_name VARCHAR(30),
+--   email VARCHAR(30) NOT NULL,
+--   token VARCHAR(30) ,
+--   password VARCHAR(30) NOT NULL
+-- );
 
 
