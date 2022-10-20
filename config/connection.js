f62fd69ec35b3a55124fa86d11049d10eb50d636
DalPenny <105093756+DalPenny@users.noreply.github.com>
2022-10-20 15:51:52 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -9,12 +9,12 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
-    }
+      port: 3306,
+    },
   );
 }
 
